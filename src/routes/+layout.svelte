16f33e3fa8a8d185203a6e72956984f509b4059c
Damien Giraudet <dgiraudet@luccasoftware.com>
2024-11-20 18:31:09 +0100

    feat(home): wip, just testing tailwinds

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,6 @@
 	let { children } = $props();
 </script>
 
-<div class="py-5">
-	<h1 class="text-center text-5xl font-bold">Aloïs Lamotte</h1>
-	<div class="h-96 bg-[url('assets/tmp-banner.png')] bg-fixed"></div>
-</div>
 <Navbar />
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1 class="text-3xl font-bold">Bonjour !</h1>
-<p>Voici mon site</p>
+<div class="flex flex-col">
+	<div class="grow ">
+		<div class="h-96 bg-[url('assets/tmp-banner.png')] bg-fixed bg-center">
+			<h1 class="py-2 text-center text-5xl font-medium">Aloïs Lamotte</h1>
+			<p>J'aime peindre et créer</p>
+			<p>Comment est votre blanquette ?</p>
+			<p>Comment est votre blanquette ?</p>
+			<p>Comment est votre blanquette ?</p>
+			<p>Comment est votre blanquette ?</p>
+			<p>Comment est votre blanquette ?</p>
+		</div>
+	</div>
+</div>
