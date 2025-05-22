64a60487c61f6bb2ad7cc8f0b9a1e1db36bfabfc
damien <damien@giraudet.me>
2025-05-23 00:07:08 +0200

    feat(pages): start to implement contact page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Century Gothic', 'CenturyGothic', 'AppleGothic', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} 
