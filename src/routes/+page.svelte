1818ea30d8dc27596e560307dc1947367b46046d
damien <damien@giraudet.me>
2025-09-26 23:11:39 +0200

    fix(intro): second try to fix for ios bellow 15.4 fullscreen first page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <!-- Intro fullscreen -->
-<div class="relative min-h-screen min-h-svh bg-cover bg-center bg-no-repeat" style="background-image: url('assets/tmp-banner.jpg');">
+<div class="min-h-screen-ios relative min-h-screen bg-cover bg-center bg-no-repeat" style="background-image: url('assets/tmp-banner.jpg');">
 	<div class="h-24"></div>
 	<div class="absolute bottom-10 flex w-full animate-bounce justify-center">
 		<svg class="h-6 w-6 text-white" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
